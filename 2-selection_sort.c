d57b7eea0e6c02978eef8a38cf7b362e0b24b7e7
Azizaazu <aynaou.aziza98@gmail.com>
2023-09-25 14:21:15 -0700

    update

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -14,24 +14,24 @@
 
 void selection_sort(int *array, size_t size)
 {
-        size_t x, stp, min_index;
-        int tmp;
+	size_t x, stp, min_index;
+	int tmp;
 
-        for (stp = 0; stp < size - 1; stp++)
-        {
-                min_index = stp;
-                for (x = stp + 1; x < size; x++)
-                {
-                        if (array[x] < array[min_index])
-                                min_index = x;
-                }
+	for (stp = 0; stp < size - 1; stp++)
+	{
+		min_index = stp;
+		for (x = stp + 1; x < size; x++)
+		{
+			if (array[x] < array[min_index])
+				min_index = x;
+		}
 
-                if (min_index != stp)
-                {
-                        tmp = array[stp];
-                        array[stp] = array[min_index];
-                        array[min_index] = tmp;
-                        print_array(array, size);
-                }
+		if (min_index != stp)
+		{
+			tmp = array[stp];
+			array[stp] = array[min_index];
+			array[min_index] = tmp;
+			print_array(array, size);
+		}
 	}
 }
